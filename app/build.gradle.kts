084a48f8e45f31899b5cf9532c5da65ce858e774
Mudassir Satti <157783267+MudassirSatti@users.noreply.github.com>
2025-08-31 23:50:23 +0500

    ** autoinspection resolved ui bugs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        setProperty("archivesBaseName", "EasyDiary-v$versionCode($versionName)")
+        setProperty("archivesBaseName", "INSPECTACAR-v$versionCode($versionName)")
     }
 
     buildTypes {
